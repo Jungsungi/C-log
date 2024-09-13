6bfc88484cfd3833916eee2a79f332281d33a9ad
sungiJung <sungi1205@naver.com>
2024-09-13 20:37:56 +0900

    [Front] 1. 공통 CSS 추가

---
+++ src/main/frontend/src/components/Fotter.js
@@ -0,0 +1,12 @@
+import "../css/Footer.css"
+
+function Fotter() {
+    return (
+        <div className="footer">
+            <p>C-log</p>
+            <p>JSG</p>
+            <p>contact : 010-5291-7736</p>
+        </div>
+    )
+}
+export default Fotter

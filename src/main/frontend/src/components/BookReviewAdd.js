317d37b53978b744ef85fab07f765d025dacc20a
sungiJung <sungi1205@naver.com>
2024-09-19 17:53:43 +0900

    [Front] 1. BookReviewAdd.js 화면 변경

--- src/main/frontend/src/components/BookReviewAdd.js
+++ src/main/frontend/src/components/BookReviewAdd.js
@@ -2,11 +2,12 @@ import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import "../css/ReviewAdd.css"
+import "../css/Common.css"
 
 function BookReviewAdd() {
     const {isbn} = useParams()
     const [item, setItem] = useState({})
-    const [activeGrade, setActiveGrade] = useState(""); // 클릭된 버튼 상태 관리
+    const [activeGrade, setActiveGrade] = useState(""); // 평점 버튼
 
     const gradeValue = ['1', '2', '3', '4', '5']
 
@@ -45,44 +46,72 @@ function BookReviewAdd() {
 
 
     return(
-        <div>
+        <div className="content-body">
             <div>
-                <h1>책 리뷰 작성</h1>
+                <h1 style={{textAlign : "left"}}>책 리뷰 작성</h1>
             </div>
-            <div>
+            <div className="item-info">
                 <div>
-                    <input type="text" id="title" name="title" value={review.title} onChange={reviewChange}/>
+                    <input style={{width: "100%", height: "50px", fontSize: "20px"}} placeholder="제목을 입력해주세요."
+                           type="text" id="title" name="title" value={review.title} onChange={reviewChange}/>
                 </div>
-                <div>
-                    <img src={item.img} />
+                <div className="review-img">
+                    <img src={item.img} alt={item.name}/>
                 </div>
-                <div className="grade-box">
-                    <span>평점</span>
-                    {gradeValue.map(grade => (
-                        <button
-                            key={grade}
-                            className={activeGrade === grade ? 'active' : ''}
-                            onClick={() => handleGradeClick(grade)}
-                        >
-                            <label htmlFor={`grade${grade}`}>{grade}</label>
-                        </button>
-                    ))}
-                    {gradeValue.map(grade => (
-                        <input
-                            key={`input${grade}`}
-                            className="d-none"
-                            id={`grade${grade}`}
-                            type="radio"
-                            name="grade"
-                            value={grade}
-                            checked={review.grade === grade}
-                            onChange={reviewChange}
-                        />
-                    ))}
+                <div className="grade-box info-box">
+                    <span><h2 className="review-tag">평점</h2></span>
+                    <span className="grade-btn">
+                        {gradeValue.map(grade => (
+                            <button
+                                key={grade}
+                                className={activeGrade === grade ? 'active' : ''}
+                                onClick={() => handleGradeClick(grade)}
+                            >
+                                <label htmlFor={`grade${grade}`}>{grade}</label>
+                            </button>
+                        ))}
+                        {gradeValue.map(grade => (
+                            <input
+                                key={`input${grade}`}
+                                className="d-none"
+                                id={`grade${grade}`}
+                                type="radio"
+                                name="grade"
+                                value={grade}
+                                checked={review.grade === grade}
+                                onChange={reviewChange}
+                            />
+                        ))}
+                    </span>
+                </div>
+                <div className="info-box review-name">
+                    <span><h2 className="review-tag">제목</h2></span>
+                    <span>
+                        <h3 style={{display: "inline", marginLeft: "20px"}}>{item.name}</h3>
+                    </span>
+                </div>
+                <div className="info-box">
+                    <span><h2 className="review-tag">작가</h2></span>
+                    <span>
+                        <h3 style={{display: "inline", marginLeft: "20px"}}>{item.author}</h3>
+                    </span>
+                </div>
+                <div className="info-box">
+                    <span><h2 className="review-tag">출판일</h2></span>
+                    <span>
+                        <h3 style={{display: "inline", marginLeft: "20px"}}>{item.pubDate}</h3>
+                    </span>
+                </div>
+                <div className="info-box">
+                    <span><h2 className="review-tag">출판사</h2></span>
+                    <span>
+                        <h3 style={{display: "inline", marginLeft: "20px"}}>{item.publisher}</h3>
+                    </span>
                 </div>
             </div>
-            <div>
-                <textarea name="content" value={review.content} onChange={reviewChange}></textarea>
+            <div className="review-info">
+                <textarea name="content" value={review.content} onChange={reviewChange}
+                          placeholder="리뷰 내용을 입력해 주세요."></textarea>
             </div>
 
             <div>
